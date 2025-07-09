83379591f4e4fa3fdf09d2e86587add33c63a284
Rahul-Jaiswal-dev <Rahul.Jaiswal@bentley.com>
2025-07-09 07:00:51 +0530

    sentry added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,21 @@ import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+
+console.log("Sentry added");
+Sentry.init({
+  dsn: "https://9119b35a60495b591a12c68ac5608dc8@o4508322765471744.ingest.de.sentry.io/4509635836444752",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.replayIntegration()
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.render(
   <ChakraProvider>
